29c1843ee908997e46e0b8e9791b7708e259e58e
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-08-31 20:57:40 +0530

    integrate database connection

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "ny-friends-system";
+
+// create database connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// check database connection errors
+if ($conn->connect_errno) die("Database connection error:".$conn->connect_error);
+
