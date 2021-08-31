2ca3c6de6f8c0dcd034e3abbb4c208a656402e4c
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-09-01 00:44:12 +0530

    implemented styles

--- friends-view.php
+++ friends-view.php
@@ -1,5 +1,5 @@
 <?php
-require ("db.php");
+require("db.php");
 session_start();
 if (!isset($_SESSION['uid'])) {
     header("location:login.php");
@@ -24,16 +24,21 @@ if (isset($_REQUEST['submit'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="style/main.css">
+
     <title>Friends</title>
-    <div class="container">
-        <table>
-            <?php
-            $uid = $_SESSION['uid'];
-            $sql = "SELECT user.id AS id,name FROM user_friend INNER JOIN user ON user_friend.id = user.id WHERE uid = {$uid}";
-            $res = $conn->query($sql);
-            if ($res->num_rows) {
-                while ($r = $res->fetch_array()) {
-                    echo "
+</head>
+<body>
+<div class="container">
+    <table>
+        <?php
+        $uid = $_SESSION['uid'];
+        $sql = "SELECT user.id AS id,name FROM user_friend INNER JOIN user ON user_friend.id = user.id WHERE uid = {$uid}";
+        $res = $conn->query($sql);
+        if ($res->num_rows) {
+            while ($r = $res->fetch_array()) {
+                echo "
                        <tr>
                            <td>{$r['name']}</td>
                            <td>
@@ -44,14 +49,11 @@ if (isset($_REQUEST['submit'])) {
                             </td>
                         </tr>
                 ";
-                }
             }
-            ?>
-        </table>
-        <a href="add-friends-view.php">Add Friends</a>
-    </div>
-</head>
-<body>
-
+        }
+        ?>
+    </table>
+    <a href="add-friends-view.php">Add Friends</a>
+</div>
 </body>
 </html>
