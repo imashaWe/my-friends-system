6929a5b416783eabe45086a0c7f11927bced819d
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-09-01 22:51:26 +0530

    update db

--- db/my-friends-system.sql
+++ db/my-friends-system.sql
@@ -28,6 +28,22 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`name`,`email`,`password`) values 
+(1,'test','test@gmail.com','202cb962ac59075b964b07152d234b70'),
+(2,'Nawodya Imasha','i@gamil.com','202cb962ac59075b964b07152d234b70'),
+(3,'Imasha Kularathne','imasha98.we@gmail.com','202cb962ac59075b964b07152d234b70'),
+(4,'Dasun Shanaka','nethminiD@gmail.com','202cb962ac59075b964b07152d234b70'),
+(5,'Dasith Upamada','dasith@gamil.com','202cb962ac59075b964b07152d234b70'),
+(6,'Chamath Geethanjana','chamath@gmail.com','202cb962ac59075b964b07152d234b70'),
+(7,'Kamal Dilshan','kamal@gamil.com','202cb962ac59075b964b07152d234b70'),
+(8,'Nadun Perera','nadun@gmail.com','202cb962ac59075b964b07152d234b70'),
+(9,'Saman Gamage','saman@gmail.com','202cb962ac59075b964b07152d234b70'),
+(10,'Nimal Perera','nirmal@gmail.com','202cb962ac59075b964b07152d234b70'),
+(11,'Jagath Akalanka','jagath@gmail.com','202cb962ac59075b964b07152d234b70'),
+(12,'Tharaka Gurusinhe','tharaka@gmail.com','202cb962ac59075b964b07152d234b70');
+
 /*Table structure for table `user_friend` */
 
 DROP TABLE IF EXISTS `user_friend`;
@@ -36,6 +52,18 @@ CREATE TABLE `user_friend` (
   `uid` int(11) DEFAULT NULL,
   `id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user_friend` */
+
+insert  into `user_friend`(`uid`,`id`) values 
+(1,3),
+(3,1),
+(1,4),
+(4,1),
+(5,4),
+(4,5),
+(3,5),
+(5,3);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
