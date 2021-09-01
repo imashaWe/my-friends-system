3a408f1594536046b39c66c44f9762c9cd6bbb76
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-09-01 12:30:32 +0530

    add about page

--- about.php
+++ about.php
@@ -1,3 +1,6 @@
+<?php
+    require ('db.php');
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,11 +8,26 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/main.css">
     <title>About</title>
 </head>
 <body>
-    <div class="container">
-
+    <div class="container border-dark">
+        <h1>About|My Friends System</h1>
+        <table>
+            <tr>
+                <td>Project Owner:</td>
+                <td>W.A.N.I.Kularathne</td>
+            </tr>
+            <tr>
+                <td>Database Server:</td>
+                <td><?=$conn->host_info?></td>
+            </tr>
+            <tr>
+                <td>Server Name:</td>
+                <td><?=$_SERVER['SERVER_NAME']?></td>
+            </tr>
+        </table>
     </div>
 </body>
 </html>
